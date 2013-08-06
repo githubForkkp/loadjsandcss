94681aacbf6886c828af4f303e069ed82974d0f3
githubForkkp <yp_kkp@msn.com>
2013-08-06 17:06:17 +0800

    update

--- loadJSAndCss.user.js
+++ loadJSAndCss.user.js
@@ -6,38 +6,41 @@
 // @include     http://detail.daily.tmall.net/item.htm*
 // @include     http://detail.tmall.com/item.htm*
 // @include     http://www.tmall.com*
+// @include     http://list.tmall.com/search_product.htm*
 // @version     1
 // ==/UserScript==
 var win = unsafeWindow || window;
 
 
 //	重写alert样式
-loadJS('https://gist.github.com/githubForkkp/6060617/raw/82b98e1d76664db1090d5622d804553c5ac66297/jquery.js',function(){
-	loadJS('https://gist.github.com/githubForkkp/6060443/raw/bca33ba68c85f71de92185fb8d0d72fb319f1765/jQuery_Alert',function(){
-		loadJS('https://gist.github.com/githubForkkp/6060625/raw/bbf69df5f31f7af4540800c5a9b92d8dbb37db05/jquery.ui.draggable.js',function(){
-			loadJS('https://gist.github.com/githubForkkp/6060631/raw/671746bcdd0a90218c6458f1062d0f606411e672/jquery.BubblePopup-1.1.min.js',function(){
-				//console.log('Js has been loaded!')
-			})
-		})
+loadJS('https://loadjscss.googlecode.com/svn/branches/jQuery.js',function(){
+	loadJS('https://loadjscss.googlecode.com/svn/branches/jQuery_Alert.js',function(){
+		loadJS('https://loadjscss.googlecode.com/svn/branches/jquery.ui.draggable.js',function(){
+      loadJS('https://loadjscss.googlecode.com/svn/branches/jquery.BubblePopup-1.1.min.js',function(){
+        // 气泡样式
+        loadJS('https://loadjscss.googlecode.com/svn/branches/jquery.qtip.js',function(){
+        
+        })
+        //console.log('Js has been loaded!')
+      })
+    })
 	})
-});
-loadCSS('https://gist.github.com/githubForkkp/6060636/raw/28fc6c23c29f7e61fd62d16c1fd08e4427dc7e06/jquery.alerts.css',function(){
-	loadCSS('https://gist.github.com/githubForkkp/6060643/raw/e9dcc783e26d471e76e638680f93dee8f84f5eae/demo.css',function(){
+})
+loadCSS('http://labs.abeautifulsite.net/archived/jquery-alerts/demo/jquery.alerts.css',function(){
+	loadCSS('https://loadjscss.googlecode.com/svn/branches/demo.css',function(){
 		//console.log('Css has been loaded!')
 	})
 	
-});
+})
 
-// 气泡样式
-loadJS('https://gist.github.com/githubForkkp/6060501/raw/5bf996ad99b4e70d0ad4c026bcbee4f9838edbe3/jquery.qtip',function(){
-        
-});
+
+
 
 
 // load外部js方法
 function loadJS(url, success) {
   var domScript = document.createElement('script');
-  domScript.type= "text/javascript"; 
+  //domScript.type= "text/javascript"; 
   domScript.src = url;
   success = success || function(){};
   domScript.onload = domScript.onreadystatechange = function() {
